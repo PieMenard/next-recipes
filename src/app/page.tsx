5f3fcb4886dac9d3331fcf6e44a53516ba5453eb
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-09-05 11:25:33 -0400

    add card component and fetch data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,49 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import Header from './components/Header';
 import Search from './components/Search';
+import { Meal } from '@/types/Meal';
+import Card from './components/Card';
 
 export default function Home() {
+  const [recipes, setRecipes] = useState<Meal[]>([]);
+  const [search, setSearch] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const fetchRecipes = async () => {
+      try {
+        const res = await fetch(
+          'https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood'
+        );
+
+        if (!res.ok) {
+          throw new Error('Something went wrong');
+        }
+        const data = await res.json();
+        setRecipes(data.meals);
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    };
+
+    fetchRecipes();
+  }, []);
+
   return (
     <div>
       <Header />;
       <Search />
+      <div className="flex items-center justify-center p-10">
+        <div className="flex flex-wrap flex-col lg:flex-row items-center gap-5">
+          {recipes?.map((recipe) => (
+            <Card recipe={recipe} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/types/Meal.ts
@@ -0,0 +1,5 @@
+export type Meal = {
+    strMeal: string,
+    strMealThumb: string,
+    idMeal: string
+}
